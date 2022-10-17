7bfb3651e4fb5578ba2ca4c7b290f26fd750b0f8
Aashish Jangam <aashish.jangam55@gmail.com>
2022-10-17 19:15:56 +0545

    test

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = document.getElementById('champselect-inline-widget')
+
+console.log(process.env)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App appName={root?.getAttribute('data-slug') || ''} />
+  </React.StrictMode>,
+  root
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

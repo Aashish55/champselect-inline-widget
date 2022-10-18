e2bed1d5b32607bf631dbab852dc684e6828b4bf
Aashish Jangam <aashish.jangam55@gmail.com>
2022-10-18 08:33:08 +0545

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ChampselectInlineWidget from './Component/ChampselectInlineWidget/ChampselectInlineWidget';
 
-// Components
-// import Widget from './Component/Widget/Widget';
-// import IntegrateWithCampminder from './Component/IntegrateWithCampminder/IntegrateWithCampminder';
 
 const App = ({ appName }: { appName: string }) => {
 
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/campminder' element={<IntegrateWithCampminder />} />
-    //     <Route path='/' element={<Widget appName={appName} />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>Hello there</div>
+    <ChampselectInlineWidget appName={appName} />
   );
 }
 
 export default App;
+

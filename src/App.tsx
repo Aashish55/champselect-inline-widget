93d4e3f96011e0e88590d6a16c3cc7a3174fe8c4
Aashish Jangam <aashish.jangam55@gmail.com>
2022-10-18 07:59:49 +0545

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Components
-import Widget from './Component/Widget/Widget';
-import IntegrateWithCampminder from './Component/IntegrateWithCampminder/IntegrateWithCampminder';
+// import Widget from './Component/Widget/Widget';
+// import IntegrateWithCampminder from './Component/IntegrateWithCampminder/IntegrateWithCampminder';
 
 const App = ({ appName }: { appName: string }) => {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/campminder' element={<IntegrateWithCampminder />} />
-        <Route path='/' element={<Widget appName={appName} />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/campminder' element={<IntegrateWithCampminder />} />
+    //     <Route path='/' element={<Widget appName={appName} />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>Hello there</div>
   );
 }
 
